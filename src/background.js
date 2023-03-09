dbf7395efd661994b57ec29e5dc0239140202591
uyume <dakerj@qq.com>
2023-03-09 21:18:25 +0800

    重新构建结构

--- src/background.js
+++ src/background.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GREETINGS') {
     const message = `Hi ${
