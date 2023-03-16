22564a5758f519ad424a23e06f09ec82291e3f7a
uyume <dakerj@qq.com>
2023-03-16 16:46:48 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,9 +6,12 @@ import jQuery from 'jquery'
 import './assets/main.css'
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import uCore from './js/core/uAnimeCore.js'
 
 const app = createApp(App)
 app.config.globalProperties.$ = jQuery
+uCore.initCore()
+app.config.globalProperties.uCore = uCore
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
